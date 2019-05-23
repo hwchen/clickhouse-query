82b891ee2f539b7f32df06d4fa98f6f6b2dfd465
Walther Chen <walther.chen@gmail.com>
2019-05-23 15:27:07 -0400

    convert to rust types

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clickhouse_rs::{Pool, types::Block};
+use clickhouse_rs::{Pool, types::{Block, SqlType}};
 use structopt::StructOpt;
 use tokio::prelude::*;
 
@@ -14,7 +14,61 @@ fn main() {
         .and_then(|c| c.ping())
         .and_then(move |c| c.query(&query).fetch_all())
         .and_then(move |(_, block)| {
-            println!("{:?}", block);
+            for row in block.rows() {
+                for i in 0..row.len() {
+                    match row.sql_type(i) {
+                        Ok(SqlType::UInt8) => {
+                            let val: u8 = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::UInt16) => {
+                            let val: u16 = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::UInt32) => {
+                            let val: u32 = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::UInt64) => {
+                            let val: u64 = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::Int8) => {
+                            let val: i8 = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::Int16) => {
+                            let val: i16 = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::Int32) => {
+                            let val: i32 = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::Int64) => {
+                            let val: i64 = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::String) => {
+                            let val: &str = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::FixedString(usize)) => {
+                            let val: &str = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::Float32) => {
+                            let val: f32 = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        Ok(SqlType::Float64) => {
+                            let val: f64 = row.get(i)?;
+                            println!("{}", val);
+                        },
+                        _ => panic!(),
+                    };
+                }
+            }
             Ok(())
         })
         .map_err(|err| println!("database error: {}", err));
