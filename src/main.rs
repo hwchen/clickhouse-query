e3ee0e82ffd58c74beb32c30ec7e08dc08b79c50
Walther Chen <walther.chen@gmail.com>
2019-05-23 16:22:47 -0400

    fix nulltype

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clickhouse_rs::{Pool, types::{Block, SqlType}};
+use clickhouse_rs::{Pool, types::SqlType};
 use structopt::StructOpt;
 use tokio::prelude::*;
 
@@ -58,7 +58,7 @@ fn main() {
                             println!("{}", val);
                         },
                         Ok(SqlType::Float32) => {
-                            let val: f64 = row.get(i)?;
+                            let val: f32 = row.get(i)?;
                             println!("{}", val);
                         },
                         Ok(SqlType::Float64) => {
@@ -68,52 +68,52 @@ fn main() {
                         Ok(SqlType::Nullable(t)) => {
                             match t {
                                 SqlType::UInt8 => {
-                                    let val: u8 = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<u8> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::UInt16 => {
-                                    let val: u16 = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<u16> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::UInt32 => {
-                                    let val: u32 = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<u32> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::UInt64 => {
-                                    let val: u64 = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<u64> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::Int8 => {
-                                    let val: i8 = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<i8> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::Int16 => {
-                                    let val: i16 = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<i16> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::Int32 => {
-                                    let val: i32 = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<i32> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::Int64 => {
-                                    let val: i64 = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<i64> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::String => {
-                                    let val: &str = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<&str> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::FixedString(_) => {
-                                    let val: &str = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<&str> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::Float32 => {
-                                    let val: f64 = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<f32> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 SqlType::Float64 => {
-                                    let val: f64 = row.get(i)?;
-                                    println!("{}", val);
+                                    let val: Option<f64> = row.get(i)?;
+                                    println!("{:?}", val);
                                 },
                                 _ => panic!(),
                             }
